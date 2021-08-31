44f7e7d4d0b0bd92c5b5203648810c916a043ab5
narcoticfresh <dn@xbe.ch>
2021-08-31 15:14:34 +0200

    initial testing

--- src/test/java/io/jenkins/plugins/jwt_auth/JwtAuthSecurityRealmTest.java
+++ src/test/java/io/jenkins/plugins/jwt_auth/JwtAuthSecurityRealmTest.java
@@ -8,103 +8,159 @@ import com.auth0.jwt.algorithms.Algorithm;
 import com.github.tomakehurst.wiremock.client.WireMock;
 import com.github.tomakehurst.wiremock.core.WireMockConfiguration;
 import com.github.tomakehurst.wiremock.junit.WireMockRule;
-import java.net.MalformedURLException;
-import java.util.concurrent.Callable;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+import com.google.common.collect.ImmutableList;
+import hudson.security.SecurityRealm;
 import jenkins.model.Jenkins;
-import org.jose4j.jwk.EcJwkGenerator;
-import org.jose4j.jwk.EllipticCurveJsonWebKey;
-import org.jose4j.jwk.JsonWebKey;
-import org.jose4j.jwk.JsonWebKeySet;
-import org.jose4j.jwk.RsaJsonWebKey;
-import org.jose4j.jwk.RsaJwkGenerator;
+import org.jose4j.jwk.*;
 import org.jose4j.jws.AlgorithmIdentifiers;
 import org.jose4j.keys.EllipticCurves;
 import org.jose4j.lang.JoseException;
-import org.junit.Before;
-import org.junit.BeforeClass;
-import org.junit.ClassRule;
-import org.junit.Rule;
-import org.junit.Test;
+import org.junit.*;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
 import org.jvnet.hudson.test.JenkinsRule;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 
+@RunWith(Parameterized.class)
 public class JwtAuthSecurityRealmTest {
 
-  @ClassRule
-  public static WireMockRule wireMockRule = new WireMockRule(WireMockConfiguration.options().port(9999));
+    @ClassRule
+    public static WireMockRule wireMockRule = new WireMockRule(WireMockConfiguration.options().port(9999));
 
-  @Rule
-  public final JenkinsRule jenkinsRule = new JenkinsRule();
+    public static RsaJsonWebKey rsaJwk;
+    public static EllipticCurveJsonWebKey ecJwk;
 
-  public static RsaJsonWebKey rsaJwk1;
-  public static RsaJsonWebKey rsaJwk2;
+    @Rule
+    public final JenkinsRule jenkinsRule = new JenkinsRule();
 
-  private Jenkins jenkins;
+    private Jenkins jenkins;
 
-  @BeforeClass
-  public static void prepare() throws JoseException {
-    rsaJwk1 = RsaJwkGenerator.generateJwk(2048);
-    rsaJwk1.setAlgorithm(AlgorithmIdentifiers.RSA_USING_SHA256);
-    rsaJwk1.setKeyId("id1");
-    rsaJwk2 = RsaJwkGenerator.generateJwk(2048);
-    rsaJwk2.setAlgorithm(AlgorithmIdentifiers.RSA_USING_SHA256);
-    rsaJwk2.setKeyId("id2");
-    EllipticCurveJsonWebKey ecJwk = EcJwkGenerator.generateJwk(EllipticCurves.P256);
+    /* test params */
+    private String headerName;
+    private String userClaimName;
+    private String groupClaimName;
+    private String groupClaimSeparator;
+    private String keyId;
+    private Algorithm algorithm;
+    private String expectedUser;
+    private List<String> expectedGroups;
 
-    JsonWebKeySet jwks = new JsonWebKeySet(rsaJwk1, rsaJwk2, ecJwk);
+    @Before
+    public void prepare() {
+        jenkins = jenkinsRule.jenkins;
+    }
 
-    String jwksJson = jwks.toJson(JsonWebKey.OutputControlLevel.PUBLIC_ONLY);
+    @Parameterized.Parameters
+    public static Collection<Object[]> data() throws JoseException {
+        rsaJwk = RsaJwkGenerator.generateJwk(2048);
+        rsaJwk.setAlgorithm(AlgorithmIdentifiers.RSA_USING_SHA256);
+        rsaJwk.setKeyId("id1");
 
-    wireMockRule.stubFor(
-        WireMock.get(WireMock.urlEqualTo("/.well-known/jwks.json"))
-            .willReturn(
-                WireMock.aResponse()
-                    .withHeader("Content-Type", "application/json")
-                    .withBody(jwksJson)
-            )
-    );
-  }
+        ecJwk = EcJwkGenerator.generateJwk(EllipticCurves.P256);
+        ecJwk.setKeyId("id2");
 
-  @Before
-  public void setUp() {
-    jenkins = jenkinsRule.jenkins;
-  }
+        JsonWebKeySet jwks = new JsonWebKeySet(rsaJwk, ecJwk);
 
-  @Test
-  public void testBasicHandling() throws Exception {
-    JwtAuthSecurityRealm realm = new JwtAuthSecurityRealm(
-      "Authorization",
-      "user",
-      "group",
-      "",
-      "http://localhost:9999/.well-known/jwks.json",
-        0,
-        false
-    );
-    jenkins.setSecurityRealm(realm);
+        String jwksJson = jwks.toJson(JsonWebKey.OutputControlLevel.PUBLIC_ONLY);
 
-    // create token
-    Algorithm algorithmRS = Algorithm.RSA256(rsaJwk1.getRsaPublicKey(), rsaJwk2.getRsaPrivateKey());
+        wireMockRule.stubFor(
+                WireMock.get(WireMock.urlEqualTo("/.well-known/jwks.json"))
+                        .willReturn(
+                                WireMock.aResponse()
+                                        .withHeader("Content-Type", "application/json")
+                                        .withBody(jwksJson)
+                        )
+        );
 
-    String token = JWT.create()
-        .withIssuer("test")
-        .withKeyId("id1")
-        .withClaim("user", "testuser")
-        .withClaim("group", "hans")
-        .sign(algorithmRS);
+        return ImmutableList.of(
+                // normal use case with rsa key
+                new Object[]{
+                        "Authorization",
+                        "username",
+                        "groups",
+                        "",
+                        "id1",
+                        Algorithm.RSA256(rsaJwk.getRsaPublicKey(), rsaJwk.getRsaPrivateKey()),
+                        "testuser",
+                        List.of("hans")
+                },
+                // normal use case with ec key
+                new Object[]{
+                        "Authorization",
+                        "username",
+                        "groups",
+                        "",
+                        "id2",
+                        Algorithm
+                        "testuser",
+                        List.of("hans")
+                }
+        );
 
-    final JenkinsRule.WebClient client = jenkinsRule.createWebClient();
-    client.addRequestHeader("Authorization", token);
+    }
 
-    final Authentication authentication = client.executeOnServer(new Callable<Authentication>() {
-      @Override
-      public Authentication call() {
-        return Jenkins.getAuthentication2();
-      }
-    });
+    public JwtAuthSecurityRealmTest(
+            String headerName,
+            String userClaimName,
+            String groupClaimName,
+            String groupClaimSeparator,
+            String keyId,
+            Algorithm signAlgo,
+            String expectedUserName,
+            List<String> expectedGroupList
+    ) {
+        this.headerName = headerName;
+        this.userClaimName = userClaimName;
+        this.groupClaimName = groupClaimName;
+        this.groupClaimSeparator = groupClaimSeparator;
+        this.keyId = keyId;
+        this.algorithm = signAlgo;
+        this.expectedUser = expectedUserName;
+        this.expectedGroups = expectedGroupList;
+    }
 
-    int hans = 3;
+    @Test
+    public void testSecurityRealm() throws Exception {
 
-  }
+        JwtAuthSecurityRealm realm = new JwtAuthSecurityRealm(
+                headerName,
+                userClaimName,
+                groupClaimName,
+                groupClaimSeparator,
+                "http://localhost:9999/.well-known/jwks.json",
+                0,
+                false
+        );
+        jenkins.setSecurityRealm(realm);
 
+        String token = JWT.create()
+                .withIssuer("test")
+                .withKeyId(keyId)
+                .withClaim(userClaimName, expectedUser)
+                .withClaim(groupClaimName, expectedGroups)
+                .sign(algorithm);
+
+        final JenkinsRule.WebClient client = jenkinsRule.createWebClient();
+        client.addRequestHeader(headerName, token);
+
+        final Authentication authentication = client.executeOnServer(Jenkins::getAuthentication2);
+
+        List<GrantedAuthority> groups = new ArrayList<>();
+        groups.add(SecurityRealm.AUTHENTICATED_AUTHORITY2);
+        for (String groupName : expectedGroups) {
+            groups.add(new SimpleGrantedAuthority(groupName));
+        }
+
+        Assert.assertEquals(expectedUser, authentication.getName());
+
+        Assert.assertEquals(groups.size(), authentication.getAuthorities().size());
+        Assert.assertTrue(groups.containsAll(authentication.getAuthorities()));
+    }
 }
